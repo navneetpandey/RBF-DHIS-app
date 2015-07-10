ab5d6a471131087f14ca2b0e0a4328166827aaeb
Navneet <navneet@ifi.uio.no>
2015-07-10 17:47:47 +0200

    small changes

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -6,7 +6,7 @@
         .controller('appController', appController);
 
     /* @ngInject */
-    function appController() {
+    function appController($http) {
         var vm = this;
         vm.title = 'RBF Application';
 
@@ -18,10 +18,27 @@
             createDataElementGroup();
         }
 
-        function createDataElementGroup($http) {
+        function createDataElementGroup() {
             var apiURL = 'http://localhost:8080/dhis/';
-            var RBF_DE_GROUP = 'RBF_DE_GROUP';
-            return $http.post(apiURL + '/api/metadata/', {organisationUnitGroups: RBF_DE_GROUP});
+            var RBF_DE_GROUP_SET = 'RBF_GROUPSET';
+            var RBF_DE_GROUP = 'RBF_GROUP';
+            window.console.log('happy');
+            $http.get(apiURL + '/api/organisationUnitGroupSets/?filter=name:eq:' + RBF_DE_GROUP_SET)
+                .then(function (orgUnitGroupSets) {
+                    if (orgUnitGroupSets.data.organisationUnitGroupSets.length === 0) {
+                        return $http.post(apiURL + '/api/organisationUnitGroupSets/',
+                            {name: RBF_DE_GROUP_SET});
+                    }
+                }).then(function () {
+                    $http.get(apiURL + '/api/organisationUnitGroups/?filter=name:eq:' + RBF_DE_GROUP)
+                        .then(function (orgUnitGroups) {
+                            if (orgUnitGroups.data.organisationUnitGroups.length === 0) {
+                                return $http.post(apiURL + '/api/organisationUnitGroups/',
+                                    {name: RBF_DE_GROUP});
+                            }
+                        });
+                }
+            );
         }
     }
 })();
